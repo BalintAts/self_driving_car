80d4840b4d92c191c47bdaeb6c4b8499d39ec397
Bálint Áts <atsbalint@gmail.com>
2020-11-19 15:19:02 +0100

    Create ObjectJsonConverter class

---
+++ Scripts/ObjectJsonConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class ObjectJsonConverter 
+{
+    public void ExportToFile(Object objectum)
+    {
+        string JsonText = CreateJson(objectum);
+        CreateAndWriteFile(JsonText);
+        string networkJson = JsonConvert.SerializeObject(objectum);
+    }
+
+    private string CreateJson(Object objectum)
+    {
+        return JsonConvert.SerializeObject(objectum);
+    }
+
+    private void CreateAndWriteFile(string JsonText)
+    {
+
+    }
+
+}
